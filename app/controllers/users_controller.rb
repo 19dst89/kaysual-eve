9aecc80490ac2cc895f9408103fe5cacf038e384
Daniel Thompson <19dst89@gmail.com>
2017-04-06 19:01:42 -0700

    user sign up with hardcoded routes for navigation, but it is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if current_user == nil
+      redirect_to root_path
+    elsif @user.id == current_user.id
+      @user = current_user
+    end
+  end
 end
