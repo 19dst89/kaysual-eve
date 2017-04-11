c9a34cde7c07628033bcbb8d1171d0617cbc7d3f
Daniel Thompson <19dst89@gmail.com>
2017-04-11 10:45:52 -0700

    forgot to add the models in the last commit after the associations were added

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  has_many :character_skills
+  has_many :toons, through: :character_skills
 end
--- app/models/toon.rb
+++ app/models/toon.rb
@@ -1,3 +1,5 @@
 class Toon < ApplicationRecord
   belongs_to :user
+  has_many :character_skills
+  has_many :skills, through: :character_skills
 end
