efa2dad0acd362fe8addc8725d85b2714cc7e571
Daniel Thompson <19dst89@gmail.com>
2017-04-12 21:10:56 -0700

    added auth to toon show page so only their user can view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root to: "users#index"
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: "users#index"
+
 
   get '/users/:id', to: 'users#show', as: 'user'
 
